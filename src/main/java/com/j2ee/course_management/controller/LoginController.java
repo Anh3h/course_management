37d48e55796c2439fa05a67f0e8cee1e617e9d76
Anh3h <courageangeh@gmail.com>
2018-06-20 04:03:46 +0100

    Configure thymleaf view

---
+++ src/main/java/com/j2ee/course_management/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.j2ee.course_management.controller;
+
+import com.j2ee.course_management.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+	public ModelAndView login(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("login");
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/registration", method = RequestMethod.GET)
+	public ModelAndView registration(){
+		ModelAndView modelAndView = new ModelAndView();
+		User user = new User();
+		modelAndView.addObject("user", user);
+		modelAndView.setViewName("registration");
+		return modelAndView;
+	}
+
+}
